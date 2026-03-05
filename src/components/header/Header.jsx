b0b174f6f1c18d88b26cb670dbe2ca27fb5831b4
FagnerTds <fagnertds@gmail.com>
2026-03-05 16:32:54 -0300

    menu mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,51 @@
-import s from "./Header.module.scss"
-import coracao from "../../assets/icones/site.png"
-import { Link } from 'react-router-dom'
+import s from "./Header.module.scss";
+import coracao from "../../assets/icones/site.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
   return (
-    <nav>
+    <header>
       <div className={s.logo}>
-      <img src={coracao} alt="imagem de um coraçao" />
-      <h2>Médicos & Dentistas</h2>
+        <img src={coracao} alt="imagem de um coraçao" />
+        <h2>Médicos & Dentistas</h2>
       </div>
-      <div className={s.botoes}>
-        <Link className={s.linkHome} to="/">Home</Link>
-        <Link className={s.linkvoluntario} to="voluntario">Seja Voluntário</Link>        
-      </div>
-    </nav>
-  )
+      <nav className={s.botoes}>
+        <Link className={s.linkHome} to="/">
+          Home
+        </Link>
+        <Link className={s.linkvoluntario} to="voluntario">
+          Seja Voluntário
+        </Link>
+      </nav>
+
+      <button
+        className={s.hamburger}
+        onClick={() => setMenuAberto(!menuAberto)}
+      >
+        ☰
+      </button>
+
+      {menuAberto && (
+        <nav className={s.menuMobile}>
+          <Link
+            className={s.linkHomeM}
+            to="/"
+            onClick={() => setMenuAberto(false)}
+          >
+            Home
+          </Link>
+          <Link
+            className={s.linkvoluntarioM}
+            to="voluntario"
+            onClick={() => setMenuAberto(false)}
+          >
+            Seja Voluntário
+          </Link>
+        </nav>
+      )}
+    </header>
+  );
 }
